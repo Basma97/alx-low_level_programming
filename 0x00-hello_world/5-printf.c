2e1ceb895b6ef786d665ea7f409b797d58dd0757
basma97 <basmamansour466@gmail.com>
2024-06-20 18:08:39 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - main function of the program
+ *
+ *Return:0 when program successfully done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
