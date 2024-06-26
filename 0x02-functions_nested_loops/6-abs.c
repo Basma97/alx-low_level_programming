06afd10b04f47fd2ecc1f29459dd20f38c080d01
basma97 <basmamansour466@gmail.com>
2024-06-25 17:14:29 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * _abs - function return the absolute value of a number
+* @a - parameter to be checked
+* Return: return 1 or 0
+*/
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
