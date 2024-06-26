2ffa8cd9f9aa852d6c10824e7a05d3d8a29999bc
basma97 <basmamansour466@gmail.com>
2024-06-26 08:35:45 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,12 +5,37 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
+	int i, j, result;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		j = i * 9;
-		_putchar (j + '0');
+		for (j = 0 ; j < 10; j++)
+		{
+			result  = i * j;
+			if (result < 10)
+			{
+				_putchar(result + '0');
+				if (j == 9)
+					continue;
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}	_putchar(' ');
+			}
+			else
+			{
+				_putchar (result / 10 + '0');
+				_putchar (result % 10 + '0');
+				if (j == 9)
+					continue;
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar ('\n');
 	}
 }
