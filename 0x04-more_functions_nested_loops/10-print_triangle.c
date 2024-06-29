afbf8927833425206ccd88ee21c177d04c59f0fd
basma97 <basmamansour466@gmail.com>
2024-06-28 18:34:43 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - function that print triangle with #
+ * @size: parameter
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i, count ,j;
+	count = 1;
+
+	for (i = 0; i < size; i++)
+	{
+		j = 0;
+		while (j < count)
+		{
+			_putchar ('#');
+			j++;
+		}
+			_putchar ('\n');
+			count++;
+	}
+}
