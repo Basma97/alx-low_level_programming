f1f1b19774f43eeaa4f654f44d799e89ad225b08
basma97 <basmamansour466@gmail.com>
2024-06-28 17:07:42 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,12 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar ('\n');
 
-	for (i = 1; i <= n ; i++)
-	{
-		for (j = 1; j <= i ; j++)
-			_putchar (' ');
-		_putchar ('\\');
-		_putchar ('\n');
-	}
+	else
+		for (i = 0; i < n ; i++)
+		{
+			for (j = 0; j <= i ; j++)
+				_putchar (' ');
+			_putchar ('\\');
+			_putchar ('\n');
+		}
 }
