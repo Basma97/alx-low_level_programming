924f71dd2956a39dc2a47a81bcb057f42201dde7
basma97 <basmamansour466@gmail.com>
2024-07-02 18:41:55 -0700

    task7 git push git commit -m task7 wq betty 7-puts_half.c clear git commit -m task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - funtion that prints half of a string
+ *@str: parameter
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, n, count = 0;
+
+	for (i = 0; str[i] != '\0' ; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1 ; str[i] != '\0' ; i++)
+		_outchar(str[i]);
+	_putchar('\n');
+}
