382c37ec5c07f8ada305aceb1aac064866668368
basma97 <basmamansour466@gmail.com>
2024-07-03 19:41:44 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates twwo strings
+ * @dest: 1st parameter
+ * @src: 2nd parameter
+ * @n: n character
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[len + i] = src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
