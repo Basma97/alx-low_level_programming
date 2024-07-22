ed12157f56745e1e5916d9782ae1bc6bd4552dfc
basma97 <basmamansour466@gmail.com>
2024-07-22 13:50:06 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct name dog
+ * @name: pointer
+ * @age: float
+ * @owner: pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
