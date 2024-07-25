b40c727e65b5c158b02d66fe59395d7db79ba729
basma97 <basmamansour466@gmail.com>
2024-07-25 07:40:51 -0700

    task3

---
+++ 0x0F-function_pointers/get_op_func.c
@@ -0,0 +1,13 @@
+#include "calc.h"
+#include "op_functions.c"
+#include <stdio.h>
+
+  op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
