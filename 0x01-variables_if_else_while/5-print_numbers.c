7f86a8ed909c995ead09d7cf6243be27085b49bc
basma97 <basmamansour466@gmail.com>
2025-02-22 08:08:30 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ * Return: zero
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+return (0);
+}
